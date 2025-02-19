7811a0b947ddec7a0f390731ea8602782b1336f0
Ali Tamer Unal <tamer.unal@icrontech.com>
2025-02-19 11:58:52 +0300

    Work on lists

--- introduction.py
+++ introduction.py
@@ -1,2 +1,14 @@
+studentIDS = [10, 25, 99, 7, "tamer"] 
+studentIDS[4] = 34.76
+
+x = studentIDS[1:3]
+
+studentIDS[1] = 12
+y = studentIDS
+
+y[2] = 33
+
+y = studentIDS[:]
+
 def f():
     return 10
