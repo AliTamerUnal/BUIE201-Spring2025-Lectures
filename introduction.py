8e6216bb0b896c8ec0dd7b16f336a920e1bf0de3
Ali Tamer Unal <tamer.unal@icrontech.com>
2025-02-24 14:44:08 +0300

    interative factorial with tests

--- introduction.py
+++ introduction.py
@@ -1,15 +1,7 @@
 
-m = 33
+def factorial(n):
+    f = 1
+    for i in range(n):
+        f *= i + 1
 
-def f(n):
-    i = n
-    print (m)
-
-    def g():
-        pass 
-
-    return g()
-
-x = f(5)
-y = f("tamer")
-print(m)
+    return f
---
+++ tests/test_factorial.py
@@ -0,0 +1,7 @@
+import introduction
+
+def test_4():
+    assert introduction.factorial(4) == 24
+    
+def test_N1():
+    assert introduction.factorial(-1) == None
