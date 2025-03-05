390f19d8f5c054e88b990d650495eb6ab8821573
Ali Tamer Unal <tamer.unal@icrontech.com>
2025-03-05 12:53:34 +0300

    classes 1

---
+++ classintro.py
@@ -0,0 +1,23 @@
+i = 5
+def f():
+    j = 10
+    print (j)
+
+f()
+class Apple:  # class, concept
+    type = "fruit"
+    location =  "tree"
+    def drop(pp):
+        #if location == "tree":
+         #   print ("I dropped")
+        #else:
+        print ("I connot be dropped")
+
+x = Apple() # object, tangible
+y = Apple()
+
+x.drop()
+
+print (x.type)
+
+
