8678b88455ae1f84ebd3d016e624ebd717419705
Ali Tamer Unal <tamer.unal@icrontech.com>
2025-03-12 14:39:46 +0300

    Latest in class discussions.

--- classintro.py
+++ classintro.py
@@ -6,17 +6,28 @@ def f():
 f()
 class Apple:  # class, concept
     type = "fruit"
-    location =  "tree"
-    def drop(pp):
-        #if location == "tree":
-         #   print ("I dropped")
-        #else:
-        print ("I connot be dropped")
+    def __init__(self, loc = "tree", clr = "red"):
+        self._location = loc
+        self._color = clr
 
-x = Apple() # object, tangible
+    def _setlocation(self, l):
+        self._location = l
+
+    def drop(self):
+        if self._location == "tree":
+            self._setlocation("ground")
+            print ("I dropped")
+        else:
+            print ("I connot be dropped")
+
+x = Apple("tree", "red") # object, tangible, implicitely calls __init__
 y = Apple()
+z = Apple(clr="yellow")
 
-x.drop()
+# z._color = "black" # access to member variable. should not be allowed.
+z.drop() # if I designate drop function as public then ok. otherwise should not be allowed.
+
+x.drop() # Actually calls Apple.drop(x)
 
 print (x.type)
 
